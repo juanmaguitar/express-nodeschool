29a0be1255447f904ebd64f17da2e9e83ba77996
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-07-17 17:56:05 +0200

    v0.5 CSS w/ Express

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 var express = require('express')
+var stylus = require('stylus');
+var path = require('path');
+
 var port = process.argv[2] || 3000;
-var bodyparser = require('body-parser')
+var srcFolder = process.argv[3] || path.join(__dirname, 'src');
 
 var app = express();
 
-app.use( bodyparser.urlencoded({ extended: false }) )
+app.set('views', srcFolder )
+app.set('view engine', 'jade')
 
-app.post('/form', function(req, res) {
-	var valueReceived = req.body.str;
-	var reversed = valueReceived.split('').reverse().join('')
-	res.end(reversed)
+app.use( stylus.middleware(srcFolder) )
+app.use( express.static(srcFolder) )
+
+app.get('/', function (req, res) {
+  res.render('index',
+  	{ title : 'Home' }
+  )
 })
+
 app.listen(port)
